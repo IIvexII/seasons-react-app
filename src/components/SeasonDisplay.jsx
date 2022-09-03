3c3d49068ad08aea88dccd696ef4daf2552c76da
IIvexII <mrh4ck3r1738@gmail.com>
2022-09-03 14:30:28 +0500

    Readme changed!

--- src/components/SeasonDisplay.jsx
+++ src/components/SeasonDisplay.jsx
@@ -1,39 +1,51 @@
 import React from 'react';
 import Seasons from '../enums/Seasons';
 
-const getSeason = (lat, month) => {
-  // Northern Hemisphere
-  if (lat > 0) {
-    // March to May -> Spring
-    if (month >= 3 && month <= 5) return Seasons.Spring;
-    // June to Augest -> Summer
-    else if (month >= 6 && month <= 8) return Seasons.Summer;
-    // September to November -> Autumn
-    else if (month >= 9 && month <= 11) return Seasons.Autumn;
-    // December to February -> Winter
-    else if (month === 12 || month <= 2) return Seasons.Winter;
-  }
-  // Southern Hemisphere
-  else if (lat < 0) {
-    // March to May -> Autumn
-    if (month >= 3 && month <= 5) return Seasons.Autumn;
-    // June to Augest -> Winter
-    else if (month >= 6 && month <= 8) return Seasons.Winter;
-    // September to November -> Spring
-    else if (month >= 9 && month <= 11) return Seasons.Spring;
-    // December to February -> Summer
-    else if (month === 12 || month <= 2) return Seasons.Summer;
-  }
-};
+export default class SeasonDisplay extends React.Component {
+  state = {
+    season: this.getSeason(this.props.lat, new Date().getMonth() + 1),
+  };
 
-export default function SeasonDisplay(props) {
-  const season = getSeason(props.lat, new Date().getMonth() + 1);
+  getSeason(lat, month) {
+    // Northern Hemisphere
+    if (lat > 0) {
+      // March to May -> Spring
+      if (month >= 3 && month <= 5) return Seasons.Spring;
+      // June to Augest -> Summer
+      else if (month >= 6 && month <= 8) return Seasons.Summer;
+      // September to November -> Autumn
+      else if (month >= 9 && month <= 11) return Seasons.Autumn;
+      // December to February -> Winter
+      else if (month === 12 || month <= 2) return Seasons.Winter;
+    }
+    // Southern Hemisphere
+    else if (lat < 0) {
+      // March to May -> Autumn
+      if (month >= 3 && month <= 5) return Seasons.Autumn;
+      // June to Augest -> Winter
+      else if (month >= 6 && month <= 8) return Seasons.Winter;
+      // September to November -> Spring
+      else if (month >= 9 && month <= 11) return Seasons.Spring;
+      // December to February -> Summer
+      else if (month === 12 || month <= 2) return Seasons.Summer;
+    }
+  }
 
-  return (
-    <div className={`season-display ${season.name.toLowerCase()}`}>
-      <i className={`icon-left massive ${season.icon} icon`} />
-      <h1>{season.phrase}</h1>
-      <i className={`icon-right massive ${season.icon} icon`} />
-    </div>
-  );
+  // componentDidMount() {
+  //   setInterval(() => {
+  //     this.setState({
+
+  //     })
+  //   },500);
+  // }
+
+  render() {
+    return (
+      <div className={`season-display ${this.state.season.name.toLowerCase()}`}>
+        <i className={`icon-left massive ${this.state.season.icon} icon`} />
+        <h1>{this.state.season.phrase}</h1>
+        <i className={`icon-right massive ${this.state.season.icon} icon`} />
+      </div>
+    );
+  }
 }
